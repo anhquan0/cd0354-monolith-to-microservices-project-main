c236c8c87a88010567219787f9df2e3372590f75
thanmahethong1622 <thanmahethong1622@gmail.com>
2023-10-15 03:38:11 +0700

    Update sequelize

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -9,10 +9,10 @@ export const sequelize = new Sequelize({
   'host': config.host,
   'dialect': config.dialect,
   'storage': ':memory:',
-  'dialectOptions': {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // added for compatibility with RDS
-    },
-  },
+  // 'dialectOptions': {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false, // added for compatibility with RDS
+  //   },
+  // },
 });
